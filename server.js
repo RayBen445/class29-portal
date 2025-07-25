a482a8328dbd5cbf0149b3f77dc8413b9f0897e8
RayBen445 <oladoyeheritage445@gmail.com>
2025-07-25 21:17:20 +0100

    Update server and add public assets

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const SECRET_KEY = "class29_secret";
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/uploads", express.static("uploads"));
+app.use(express.static("public"));
 
 const storage = multer.diskStorage({
   destination: "uploads/",
